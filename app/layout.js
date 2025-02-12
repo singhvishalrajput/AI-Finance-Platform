35ff16724a876c0c50ea81f97ea2fafc0051d133
Vishal Singh <singh287686@gmail.com>
2025-02-13 00:41:10 +0530

    Fix: Add font subset configuration

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from 'sonner'
-const inter = Inter({subset:["latin"]});
+const inter = Inter({subsets:["latin"]});
 
 export const metadata = {
   title: "Welth",
